d60c7971a067d506c5f8c9cd9b40abae47c6835e
donglulu09 <dong07007116@126.com>
2019-09-26 16:44:25 +0800

    20190926 修改位置数据为double

--- testRestApi/src/main/java/com/efo/controller/TaxiController.java
+++ testRestApi/src/main/java/com/efo/controller/TaxiController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 @RestController
 public class TaxiController {
@@ -21,7 +24,12 @@ public class TaxiController {
                 AppConfig appConfig = new AppConfig();
                 appConfig.setTitle("chuzuche_dongyc");
                 Config  config = new Config();
-                config.setLocation("[[11,22],[22,33]");
+                double[] a1 = new double[]{22.00, 33};
+                double[] a2 = new double[]{33, 44.00};
+                ArrayList<double[]> temp = new ArrayList<double[]>();
+                temp.add(a1);
+                temp.add(a2);
+                config.setLocation(temp);
                 config.setMapUrl("http://192.168.0.0:8080");
                 appConfig.setConfig(config);
                 res  = JSON.toJSONString(appConfig);
@@ -30,16 +38,11 @@ public class TaxiController {
             {
                 res = "参数传递异常 ";
             }
-
-
-
         }
         catch (Exception ex)
         {
             res ="appconfig 接口异常 ";
-
         }
-
         return  res;
     }
 }
--- testRestApi/src/main/java/com/efo/entity/Config.java
+++ testRestApi/src/main/java/com/efo/entity/Config.java
@@ -3,10 +3,12 @@ package com.efo.entity;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Data
 public  class Config  implements Serializable
 {
     private String mapUrl;
-    private String location;
+    private ArrayList<double[]> location;
 }
